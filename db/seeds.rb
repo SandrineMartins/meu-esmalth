ab37f2fa250e96acc449c5bc25153c5b4c39dbac
SandrineMartins <svm2@cin.ufpe.br>
2015-09-24 11:33:39 -0300

    inserção de nova foto

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 foto1 = FotoEsmalte.create(url:"http://www.tortugan.com.br/lojas/00020405/prod/esmalte.jpg")
 foto2 = FotoEsmalte.create(url: "http://www.sitedebelezaemoda.com.br/wp-content/uploads/2014/12/esmalte-caseiro1.jpg")
+foto3 = FotoEsmalte.create(url: 'http://www.makeupatelier.com.br/wp-content/uploads/2014/02/esmalte_maybelline_fuchsia.jpg')
 
 Comentario.create(descricao: 'vermelho, colorama', foto_esmalte:foto1)
 Comentario.create(descricao: 'rosa', foto_esmalte:foto2)
 Comentario.create(descricao: 'risque', foto_esmalte:foto2)
+Comentario.create(descricao: 'avon' 'rosa', foto_esmalte:foto3)
